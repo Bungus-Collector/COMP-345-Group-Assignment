dbde20f29ffe4c583414456be6ea32631825493a
PaulYH <paulhumenny@icloud.com>
2025-11-11 19:49:09 -0500

    Rename

--- MainDriver.cpp
+++ MainDriver.cpp
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
 {
     testCommandProcessor(argc, argv);
 
-    //testGameStartupPhase();
+    //testMainGameLoop();
 
     // testMainGameLoop() <-- coverd in testCommadnProcessor()
 
